ee6444085446a642331f9a033549b04570bfbb7f
bosuly <melendresulysses@g.cjc.edu.ph>
2024-11-13 19:55:04 -1200

    Add files via upload

---
+++ Snd.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package snd;
+
+/**
+ *
+ * @author MELENDRES_COMP111
+ */import javax.swing.JOptionPane;
+public class Snd {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+     }}
+    
+    
+
